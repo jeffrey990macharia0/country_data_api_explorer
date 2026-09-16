e3dee8e8c3463a3e5fa0bfa6f1d0f59d68b13fc3
jeffrey990macharia0 <jeffreymacharia21@gmail.com>
2026-09-16 06:35:31 -0400

    Added the code to query the restcountries API and return a list of countries to the user

--- app.py
+++ app.py
@@ -6,7 +6,20 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-	return render_template('index.html')
+	#Query to list all countries
+	response = requests.get(
+		'https://api.restcountries.com/countries/v5?limit=100',
+		headers={'Authorization': 'Bearer rc_live_819d4a5ec446428a890b8d09546bccb1'}
+	)
+	data = response.json()
+	country_list=[]
+
+	for country in data["data"]["objects"]:
+		country_name=country["names"]["common"]
+		country_list.append(country_name)
+
+	return render_template('index.html', country_list=country_list)
+
 
 if __name__ == "__main__":
 	port = int(os.environ.get("PORT", 5000))
